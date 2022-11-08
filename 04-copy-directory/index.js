bff659d3e0c42c215fa418ca906be6d1364cb2be
LineCoran <vppisya@gmail.com>
2022-11-08 19:02:29 +0300

    fix: add fs.rm for refresh dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,37 @@
 const path = require('path')
 const fs = require('fs');
 
-fs.promises.mkdir(
-    path.join(__dirname, "files-copy"),
-    {recursive: true}
-)
 
-fs.readdir(
-    path.join(__dirname, 'files'),
-    {withFileTypes: true},
-    (err, files) => {
-        if(err) {
-            console.log(err);
-            throw err;
-        }
-        files.forEach((file) => {
-            fs.copyFile(
-                path.join(__dirname, 'files', file.name),
-                path.join(__dirname, 'files-copy', file.name),
-                (err) => {
-                    if(err) throw err
+    fs.rm(
+    path.join(__dirname, 'files-copy'), 
+    { recursive: true, force: true },
+    (err) => {
+        if(err) throw err;
+        fs.promises.mkdir(
+            path.join(__dirname, "files-copy"),
+            {recursive: true}
+        )
+        fs.readdir(
+            path.join(__dirname, 'files'),
+            {withFileTypes: true},
+            (err, files) => {
+                if(err) {
+                    console.log(err);
+                    throw err;
                 }
-            )
-        })
+                files.forEach((file) => {
+                    fs.copyFile(
+                        path.join(__dirname, 'files', file.name),
+                        path.join(__dirname, 'files-copy', file.name),
+                        (err) => {
+                            if(err) throw err
+                        }
+                    )
+                })
+            }
+        )
+    
     }
-)
+    );
+
+    
